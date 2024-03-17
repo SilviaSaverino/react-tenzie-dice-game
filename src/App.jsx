61fd6847917468630b9d228b0c4d6a5afc42d5a3
Silvia Saverino <silviasave89@gmail.com>
2024-03-17 22:55:14 +0000

    add createNewDiceSet function to stay DRY, and add functionality to let rollDice function skip the selected dice when rolling a new set

--- src/App.jsx
+++ src/App.jsx
@@ -7,32 +7,38 @@ function App() {
 
   const [newDice, setNewDice] = useState(getNewDice())
   
+  function createNewDiceSet(){
+    return {
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      id: nanoid()
+    }
+  }
+
   function getNewDice(){
     const newDiceSet = []
-    for(let i = 0;i < 10; i++){
-      newDiceSet.push({
-        value: Math.ceil(Math.random() * 6),
-        isHeld: false,
-        id: nanoid()
-      })
-    }
+    for (let i = 0; i < 10; i++) {
+      newDiceSet.push(createNewDiceSet())
+      }
     return newDiceSet
   }
   
-  function rollDice(){
-    setNewDice(getNewDice())
-  }
+  function rollDice() {
+    setNewDice(oldDice => oldDice.map(dieToBeSkipped => {
+      return dieToBeSkipped.isHeld === true ? dieToBeSkipped : createNewDiceSet()
+  }))
+    }
 
-  function selectDieOnHold(id){
+  function selectDieOnHold(id) {
     setNewDice(oldDice => oldDice.map(selectedDie => {
-      return selectedDie.id === id ? {...selectedDie, isHeld: !selectedDie.isHeld} : selectedDie
-     }))
+      return selectedDie.id === id ? { ...selectedDie, isHeld: !selectedDie.isHeld } : selectedDie
+    }))
   
   }
 
   const diceElements = newDice.map(singleDie => (
-    <Die 
-      value={singleDie.value} 
+    <Die
+      value={singleDie.value}
       key={singleDie.id} 
       isHeld={singleDie.isHeld}
       selectDieOnHold={() => selectDieOnHold(singleDie.id)}
