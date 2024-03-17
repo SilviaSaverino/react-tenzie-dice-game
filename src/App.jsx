6225c94d4793fd2eca3d9ee7956d07b8ad0b50d7
Silvia Saverino <silviasave89@gmail.com>
2024-03-17 23:00:50 +0000

    add instructions and title and style them

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
-import {nanoid} from 'nanoid'
+import { nanoid } from 'nanoid'
 import './App.css'
 import Die from "./assets/components/Die"
 
 function App() {
 
   const [newDice, setNewDice] = useState(getNewDice())
-  
-  function createNewDiceSet(){
+
+
+  function createNewDiceSet() {
     return {
       value: Math.ceil(Math.random() * 6),
       isHeld: false,
@@ -15,38 +16,42 @@ function App() {
     }
   }
 
-  function getNewDice(){
+  function getNewDice() {
     const newDiceSet = []
     for (let i = 0; i < 10; i++) {
       newDiceSet.push(createNewDiceSet())
-      }
+    }
     return newDiceSet
   }
-  
+
   function rollDice() {
     setNewDice(oldDice => oldDice.map(dieToBeSkipped => {
       return dieToBeSkipped.isHeld === true ? dieToBeSkipped : createNewDiceSet()
-  }))
-    }
+    }))
+  }
+
 
   function selectDieOnHold(id) {
     setNewDice(oldDice => oldDice.map(selectedDie => {
       return selectedDie.id === id ? { ...selectedDie, isHeld: !selectedDie.isHeld } : selectedDie
     }))
-  
   }
 
   const diceElements = newDice.map(singleDie => (
     <Die
       value={singleDie.value}
-      key={singleDie.id} 
+      key={singleDie.id}
       isHeld={singleDie.isHeld}
       selectDieOnHold={() => selectDieOnHold(singleDie.id)}
-      />
-      ))
-      
+    />
+  ))
   return (
     <main>
+      <div className='info'>
+        <h1 className="title">Tenzies</h1>
+        <p className="instructions">Roll until all dice are the same. </p>
+        <p className="instructions"> Click each die to freeze it at its current value between rolls.</p>
+      </div>
       <div className='die-container'>
         {diceElements}
       </div>
